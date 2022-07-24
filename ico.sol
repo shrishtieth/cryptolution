f1409f5b63b142e84d7afd77d7cc2f99ab8323de
shrishti.eth <87112793+shrishtieth@users.noreply.github.com>
2022-07-24 17:33:47 +0530

    Update ico.sol

--- ico.sol
+++ ico.sol
@@ -25,24 +25,24 @@ interface Vesting{
 contract AstorTokenICO is Ownable {
     using SafeMath for uint256;
     using SafeERC20 for IERC20;
-    IERC20 private astor;
 
-    uint256 public startTime;
+    address public astor = 0xBfFBAe1B09d5655F78481880Cb2e054533A78d96;
+    uint256 public startTime = 1658661183;
     uint256 public amountRaised;
-    address private treasury;
-    address public referalContract;
-    address public vestingContract;
-    uint256 private tokensSold;
-    address public busd;
-    address public wbnb;
-    address public bnbPriceOracle;
-    address public busdPriceOracle;
+    address public treasury = 0x9CfCD3D329549D9A327114F5ABf73637d13eFD07 ;
+    address public referalContract = 0x0f04c7Ba8D985496173Dd97aDF165128f38309AB;
+    address public vestingContract = 0x4a597EfB591910a03f7753d472c0E982A6FDE80F;
+    uint256 public tokensSold;
+    address public busd = 0xeab9B1c602FBcAcB07F7875fE5f49Ec731f7B2D3;
+    address public wbnb = 0x9CfCD3D329549D9A327114F5ABf73637d13eFD07;
+    address public bnbPriceOracle = 0x8e1164E6D4B842728DdE5075bB6847232B4B5AA4;
+    address public busdPriceOracle = 0x573FE2ad7425A91644c7eC65a569B3487019Ae3D;
 
-    uint256  private phase1Supply = 60000000000000000000000000;
-    uint256  private phase2Supply = 120000000000000000000000000;
-    uint256  private phase3Supply = 180000000000000000000000000;
-    uint256  private phase4Supply = 330000000000000000000000000;
-    uint256  private phase5Supply = 502900000000000000000000000;
+    uint256  public phase1Supply = 60000000000000000000000000;
+    uint256  public phase2Supply = 120000000000000000000000000;
+    uint256  public phase3Supply = 180000000000000000000000000;
+    uint256  public phase4Supply = 330000000000000000000000000;
+    uint256  public phase5Supply = 502900000000000000000000000;
 
     mapping(address => uint256) public tokenBoughtUser;
     mapping(address => bool) isWhitelisted;
@@ -73,17 +73,19 @@ contract AstorTokenICO is Ownable {
     event ContractsUpdated(address referalContract, address vestingContract,
     address bnbPriceOracle, address busdPriceOracle);
 
-  
-    constructor(
-        address astorToken,
-        uint256 _start, address _treasury
+    event WhitelistUpdated(address user, bool isWhitelisted);
 
-    ) {
+  
+    // constructor(
+    //     address astorToken,
+    //     uint256 _start, address _treasury
+
+    // ) {
       
-        astor = IERC20(astorToken);
-        startTime = _start;
-        treasury = _treasury;
-    }
+    //     astor = (astorToken);
+    //     startTime = _start;
+    //     treasury = _treasury;
+    // }
 
     function updateSupply(uint256 _phase1Supply ,uint256 _phase2Supply,
     uint256 _phase3Supply, uint256 _phase4Supply, uint256 _phase5Supply) external onlyOwner{
@@ -114,6 +116,11 @@ contract AstorTokenICO is Ownable {
         treasury = _treasury;
 
         emit TreasuryUpdated( _treasury);
+    }
+
+    function updateWhitelist(address user, bool _isWhitelisted) external onlyOwner{
+        isWhitelisted[user] = _isWhitelisted;
+        emit WhitelistUpdated(user, _isWhitelisted);
     }
 
     function updateFirstBuy(uint256 amount, uint256 time) external onlyOwner{
@@ -158,6 +165,9 @@ contract AstorTokenICO is Ownable {
 
     function buyToken(address token, uint256 amount) external payable {
         require(token == wbnb || token == busd,"Invalid currency");
+        if(token == wbnb){
+            amount = msg.value;
+        }
         uint256 stage = getStage();
         require(stage > 0, "ICO has not started yet");
         uint256 price;
@@ -167,9 +177,10 @@ contract AstorTokenICO is Ownable {
                 require(isWhitelisted[msg.sender] ||
                  Referal(referalContract).isReferred(msg.sender),"Not Eligible, try later");
                 require(phase1Bought[msg.sender] == false,"Already Bought Tokens");
-                phase1Bought[msg.sender] == true;
+                phase1Bought[msg.sender] = true;
                 uint256 currencyPrice = getPrice(token);
-                amount = (firstBuyAmount*10**18/currencyPrice);               
+                amount = (firstBuyAmount*10**18/currencyPrice);   
+
             }
         }
         else if(stage == 2){
@@ -178,7 +189,7 @@ contract AstorTokenICO is Ownable {
         else if(stage == 3){
             price = phase3Price;
         }
-        else if(stage == 5){
+        else if(stage == 4){
             price = phase4Price;
         }
         else if(stage == 5){
@@ -192,8 +203,10 @@ contract AstorTokenICO is Ownable {
         }
         else if(token == wbnb){
             payable(treasury).transfer(amount); 
+            refundIfOver(amount);
         }
         Referal(referalContract).updateReward(msg.sender, usdAmount);
+        IERC20(astor).transfer(vestingContract,tokenAmount);
         Vesting(vestingContract).vestTokens(msg.sender, tokenAmount, stage);
         emit TokensBought(msg.sender, usdAmount, tokenAmount);
 
@@ -211,8 +224,39 @@ contract AstorTokenICO is Ownable {
     function getTokensForPrice(address token, uint256 amount, uint256 price)
      public view returns(uint256 tokenAmount, uint256 usdPrice){
         uint256 currencyPrice = getPrice(token);
-        tokenAmount == (currencyPrice*amount)/price;
+        tokenAmount = (currencyPrice*amount)/price;
         usdPrice = currencyPrice*amount;
-    }                                                                                                                   
+    } 
+
+    function refundIfOver(uint256 price) private {
+    if (msg.value > price) {
+      payable(msg.sender).transfer(msg.value - price);
+    }
+    }  
+
+    receive() external payable {}
+
+    /*
+    @param token address of token to be withdrawn
+    @param wallet wallet that gets the token
+    */
+
+    function withdrawTokens(IERC20 token, address wallet) external onlyOwner{
+        uint256 balanceOfContract = token.balanceOf(address(this));
+        token.transfer(wallet,balanceOfContract);
+    }
+
+     /*
+    @param wallet address that gets the Eth
+     */
+    
+    function withdrawFunds(address wallet) external onlyOwner{
+        uint256 balanceOfContract = address(this).balance;
+        payable(wallet).transfer(balanceOfContract);
+    }        
+
+    function updateSupply(uint256 token) external {
+        tokensSold = token;
+    }                                                                                                        
 
 }
