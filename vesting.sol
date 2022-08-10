551725aaf72a11bfbb61f02beda7e9ae7604815f
shrishti.eth <87112793+shrishtieth@users.noreply.github.com>
2022-08-10 11:12:25 +0530

    Update vesting.sol

--- vesting.sol
+++ vesting.sol
@@ -1,10 +1,197 @@
     // SPDX-License-Identifier: MIT
     pragma solidity 0.8.9;
 
-    import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/master/contracts/token/ERC20/IERC20.sol";
-    import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/master/contracts/access/Ownable.sol";
-    import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/master/contracts/utils/Counters.sol";
+interface IERC20 {
+    /**
+     * @dev Emitted when `value` tokens are moved from one account (`from`) to
+     * another (`to`).
+     *
+     * Note that `value` may be zero.
+     */
+    event Transfer(address indexed from, address indexed to, uint256 value);
 
+    /**
+     * @dev Emitted when the allowance of a `spender` for an `owner` is set by
+     * a call to {approve}. `value` is the new allowance.
+     */
+    event Approval(address indexed owner, address indexed spender, uint256 value);
+
+    /**
+     * @dev Returns the amount of tokens in existence.
+     */
+    function totalSupply() external view returns (uint256);
+
+    /**
+     * @dev Returns the amount of tokens owned by `account`.
+     */
+    function balanceOf(address account) external view returns (uint256);
+
+    /**
+     * @dev Moves `amount` tokens from the caller's account to `to`.
+     *
+     * Returns a boolean value indicating whether the operation succeeded.
+     *
+     * Emits a {Transfer} event.
+     */
+    function transfer(address to, uint256 amount) external returns (bool);
+
+    /**
+     * @dev Returns the remaining number of tokens that `spender` will be
+     * allowed to spend on behalf of `owner` through {transferFrom}. This is
+     * zero by default.
+     *
+     * This value changes when {approve} or {transferFrom} are called.
+     */
+    function allowance(address owner, address spender) external view returns (uint256);
+
+    /**
+     * @dev Sets `amount` as the allowance of `spender` over the caller's tokens.
+     *
+     * Returns a boolean value indicating whether the operation succeeded.
+     *
+     * IMPORTANT: Beware that changing an allowance with this method brings the risk
+     * that someone may use both the old and the new allowance by unfortunate
+     * transaction ordering. One possible solution to mitigate this race
+     * condition is to first reduce the spender's allowance to 0 and set the
+     * desired value afterwards:
+     * https://github.com/ethereum/EIPs/issues/20#issuecomment-263524729
+     *
+     * Emits an {Approval} event.
+     */
+    function approve(address spender, uint256 amount) external returns (bool);
+
+    /**
+     * @dev Moves `amount` tokens from `from` to `to` using the
+     * allowance mechanism. `amount` is then deducted from the caller's
+     * allowance.
+     *
+     * Returns a boolean value indicating whether the operation succeeded.
+     *
+     * Emits a {Transfer} event.
+     */
+    function transferFrom(
+        address from,
+        address to,
+        uint256 amount
+    ) external returns (bool);
+}
+abstract contract Context {
+    function _msgSender() internal view virtual returns (address) {
+        return msg.sender;
+    }
+
+    function _msgData() internal view virtual returns (bytes calldata) {
+        return msg.data;
+    }
+}
+
+
+/**
+ * @dev Contract module which provides a basic access control mechanism, where
+ * there is an account (an owner) that can be granted exclusive access to
+ * specific functions.
+ *
+ * By default, the owner account will be the one that deploys the contract. This
+ * can later be changed with {transferOwnership}.
+ *
+ * This module is used through inheritance. It will make available the modifier
+ * `onlyOwner`, which can be applied to your functions to restrict their use to
+ * the owner.
+ */
+abstract contract Ownable is Context {
+    address private _owner;
+
+    event OwnershipTransferred(address indexed previousOwner, address indexed newOwner);
+
+    /**
+     * @dev Initializes the contract setting the deployer as the initial owner.
+     */
+    constructor() {
+        _transferOwnership(_msgSender());
+    }
+
+    /**
+     * @dev Throws if called by any account other than the owner.
+     */
+    modifier onlyOwner() {
+        _checkOwner();
+        _;
+    }
+
+    /**
+     * @dev Returns the address of the current owner.
+     */
+    function owner() public view virtual returns (address) {
+        return _owner;
+    }
+
+    /**
+     * @dev Throws if the sender is not the owner.
+     */
+    function _checkOwner() internal view virtual {
+        require(owner() == _msgSender(), "Ownable: caller is not the owner");
+    }
+
+    /**
+     * @dev Leaves the contract without owner. It will not be possible to call
+     * `onlyOwner` functions anymore. Can only be called by the current owner.
+     *
+     * NOTE: Renouncing ownership will leave the contract without an owner,
+     * thereby removing any functionality that is only available to the owner.
+     */
+    function renounceOwnership() public virtual onlyOwner {
+        _transferOwnership(address(0));
+    }
+
+    /**
+     * @dev Transfers ownership of the contract to a new account (`newOwner`).
+     * Can only be called by the current owner.
+     */
+    function transferOwnership(address newOwner) public virtual onlyOwner {
+        require(newOwner != address(0), "Ownable: new owner is the zero address");
+        _transferOwnership(newOwner);
+    }
+
+    /**
+     * @dev Transfers ownership of the contract to a new account (`newOwner`).
+     * Internal function without access restriction.
+     */
+    function _transferOwnership(address newOwner) internal virtual {
+        address oldOwner = _owner;
+        _owner = newOwner;
+        emit OwnershipTransferred(oldOwner, newOwner);
+    }
+}
+library Counters {
+    struct Counter {
+        // This variable should never be directly accessed by users of the library: interactions must be restricted to
+        // the library's function. As of Solidity v0.5.2, this cannot be enforced, though there is a proposal to add
+        // this feature: see https://github.com/ethereum/solidity/issues/4637
+        uint256 _value; // default: 0
+    }
+
+    function current(Counter storage counter) internal view returns (uint256) {
+        return counter._value;
+    }
+
+    function increment(Counter storage counter) internal {
+        unchecked {
+            counter._value += 1;
+        }
+    }
+
+    function decrement(Counter storage counter) internal {
+        uint256 value = counter._value;
+        require(value > 0, "Counter: decrement overflow");
+        unchecked {
+            counter._value = value - 1;
+        }
+    }
+
+    function reset(Counter storage counter) internal {
+        counter._value = 0;
+    }
+}
 
 
     contract Vesting is  Ownable {
@@ -34,6 +221,11 @@
     uint256 public phase5VestTime = 5;
     uint256 public advisorVestTime = 24;
     uint256 public teamVestTime = 48;
+    uint256 public advisorAmount = 60000000000000000000000000;
+    uint256 public teamAmount = 287090571000000000000000000;
+    uint256 public advisorAmountAdded;
+    uint256 public teamAmountAdded;
+    
     address[] public allUsers;
     mapping(address => bool) public added;
 
@@ -55,7 +247,10 @@
     
     event Vested(uint256 indexed id, address indexed user);
     event Unvested(uint256 indexed id, uint256 amount);
-    // constructor() {}
+    event TgeAmountReleased(address indexed user, uint256 amount);
+    constructor(address token) {
+       astorToken = token;
+    }
 
 
     function vestTokenIco(address user, uint256 amount, uint256 phase) public returns(uint256 id){
@@ -63,6 +258,7 @@
         if(added[msg.sender] == false){
            allUsers.push(msg.sender);
         }
+        vestingID.increment();
         id = vestingID.current();
         (uint256 tgeAmount, uint256 vestAmount, uint256 releasePerEpoch, uint256 endTime) = getAmounts(amount, phase);
         userTgeAmount[user] += tgeAmount;
@@ -81,7 +277,6 @@
         isActive : true
         });
         userVests[user].push(id);
-        vestingID.increment();
         emit Vested(id, user);
         return(id);
     }
@@ -163,6 +358,88 @@
     function updateAllowed(address user, bool allowed) external onlyOwner{
         allowedToCall[user] = allowed;
     }
+
+    function distributeTgeAmount() external onlyOwner{
+       uint256 totalUsers = allUsers.length;
+       for(uint256 i =0; i< totalUsers; i++){
+          if(userTgeAmount[allUsers[i]]>0){
+           IERC20(astorToken).transfer(allUsers[i],userTgeAmount[allUsers[i]]);
+           tgeAmountReleased[allUsers[i]] += userTgeAmount[allUsers[i]];
+           totalTgeAmountReleased += userTgeAmount[allUsers[i]];
+           emit TgeAmountReleased(allUsers[i],userTgeAmount[allUsers[i]]);
+          }
+       }
+   }
+
+   function addAdvisorVest(address user, uint256 amount) external onlyOwner{
+   require(advisorAmountAdded + amount <= advisorAmount,"Limit Exceeded");
+   advisorAmountAdded += amount;
+   if(added[msg.sender] == false){
+           allUsers.push(msg.sender);
+        }
+        vestingID.increment();
+        uint256 id = vestingID.current();
+        totalTokensVested += amount;
+        idToVesting[id] = VestingDetails({
+        tokensDeposited : amount,
+        tokensWithdrawn: 0,
+        startTime : advisorCliff + block.timestamp,
+        endTime : advisorCliff + block.timestamp + advisorVestTime * 2630000,
+        releasePerEpoch : amount/advisorVestTime,
+        epoch : 2630000,
+        owner : user,
+        phase : 0,
+        lockId : id,
+        isActive : true
+        });
+        userVests[user].push(id);
+        emit Vested(id, user);
+
+   } 
+
+   function addTeamVest(address user, uint256 amount) external onlyOwner{
+   require(teamAmountAdded + amount <= teamAmount,"Limit Exceeded");
+   teamAmountAdded += amount;
+   if(added[msg.sender] == false){
+           allUsers.push(msg.sender);
+        }
+        vestingID.increment();
+        uint256 id = vestingID.current();
+        totalTokensVested += amount;
+        idToVesting[id] = VestingDetails({
+        tokensDeposited : amount,
+        tokensWithdrawn: 0,
+        startTime : teamCliff + block.timestamp,
+        endTime : teamCliff + block.timestamp + teamVestTime * 2630000,
+        releasePerEpoch : amount/teamVestTime,
+        epoch : 2630000,
+        owner : user,
+        phase : 0,
+        lockId : id,
+        isActive : true
+        });
+        userVests[user].push(id);
+        emit Vested(id, user);
+
+   } 
+
+   function amountUnlocked(address user) external view returns(uint256 amount){
+       uint256 totalVests = userVests[user].length;
+       for(uint256 i =0; i< totalVests;i++){
+         if(idToVesting[userVests[user][i]].isActive){
+            uint256 endTimestamp;
+        if(block.timestamp > idToVesting[i].endTime){
+           endTimestamp = idToVesting[i].endTime;
+        }
+        else{
+           endTimestamp = block.timestamp;
+        }
+        uint256 eligibleEpoch = (endTimestamp - idToVesting[i].startTime)/ idToVesting[i].epoch;
+        amount += (eligibleEpoch * idToVesting[i].releasePerEpoch) - 
+        idToVesting[i].tokensWithdrawn;
+          }
+       }
+   }
 
 
 
