4ed1f01e88ccbc292b49af2060bdfedfeb358f6d
shrishti.eth <87112793+shrishtieth@users.noreply.github.com>
2022-09-07 22:18:00 +0530

    Update ico.sol

--- ico.sol
+++ ico.sol
@@ -23,6 +23,356 @@ interface Vesting{
     function vestTokens(address user, uint256 amount, uint256 phase) external;
 }
 
+interface IUniswapV2Factory {
+    event PairCreated(
+        address indexed token0,
+        address indexed token1,
+        address pair,
+        uint256
+    );
+
+    function feeTo() external view returns (address);
+
+    function feeToSetter() external view returns (address);
+
+    function getPair(address tokenA, address tokenB)
+        external
+        view
+        returns (address pair);
+
+    function allPairs(uint256) external view returns (address pair);
+
+    function allPairsLength() external view returns (uint256);
+
+    function createPair(address tokenA, address tokenB)
+        external
+        returns (address pair);
+
+    // function setFeeTo(address) external;
+    // function setFeeToSetter(address) external;
+}
+
+
+
+interface IUniswapV2Pair {
+    event Approval(
+        address indexed owner,
+        address indexed spender,
+        uint256 value
+    );
+    event Transfer(address indexed from, address indexed to, uint256 value);
+
+    function name() external pure returns (string memory);
+
+    function symbol() external pure returns (string memory);
+
+    function decimals() external pure returns (uint8);
+
+    function totalSupply() external view returns (uint256);
+
+    function balanceOf(address owner) external view returns (uint256);
+
+    function allowance(address owner, address spender)
+        external
+        view
+        returns (uint256);
+
+    function approve(address spender, uint256 value) external returns (bool);
+
+    function transfer(address to, uint256 value) external returns (bool);
+
+    function transferFrom(
+        address from,
+        address to,
+        uint256 value
+    ) external returns (bool);
+
+    function DOMAIN_SEPARATOR() external view returns (bytes32);
+
+    function PERMIT_TYPEHASH() external pure returns (bytes32);
+
+    function nonces(address owner) external view returns (uint256);
+
+    function permit(
+        address owner,
+        address spender,
+        uint256 value,
+        uint256 deadline,
+        uint8 v,
+        bytes32 r,
+        bytes32 s
+    ) external;
+
+    event Mint(address indexed sender, uint256 amount0, uint256 amount1);
+    event Burn(
+        address indexed sender,
+        uint256 amount0,
+        uint256 amount1,
+        address indexed to
+    );
+    event Swap(
+        address indexed sender,
+        uint256 amount0In,
+        uint256 amount1In,
+        uint256 amount0Out,
+        uint256 amount1Out,
+        address indexed to
+    );
+    event Sync(uint112 reserve0, uint112 reserve1);
+
+    function MINIMUM_LIQUIDITY() external pure returns (uint256);
+
+    function factory() external view returns (address);
+
+    function token0() external view returns (address);
+
+    function token1() external view returns (address);
+
+    function getReserves()
+        external
+        view
+        returns (
+            uint112 reserve0,
+            uint112 reserve1,
+            uint32 blockTimestampLast
+        );
+
+    function price0CumulativeLast() external view returns (uint256);
+
+    function price1CumulativeLast() external view returns (uint256);
+
+    function kLast() external view returns (uint256);
+
+    function mint(address to) external returns (uint256 liquidity);
+
+    function burn(address to)
+        external
+        returns (uint256 amount0, uint256 amount1);
+
+    function swap(
+        uint256 amount0Out,
+        uint256 amount1Out,
+        address to,
+        bytes calldata data
+    ) external;
+
+    function skim(address to) external;
+
+    function sync() external;
+
+    function initialize(address, address) external;
+}
+
+
+
+interface IUniswapV2Router01 {
+    function factory() external pure returns (address);
+
+    function WETH() external pure returns (address);
+
+    function addLiquidity(
+        address tokenA,
+        address tokenB,
+        uint256 amountADesired,
+        uint256 amountBDesired,
+        uint256 amountAMin,
+        uint256 amountBMin,
+        address to,
+        uint256 deadline
+    )
+        external
+        returns (
+            uint256 amountA,
+            uint256 amountB,
+            uint256 liquidity
+        );
+
+    function addLiquidityETH(
+        address token,
+        uint256 amountTokenDesired,
+        uint256 amountTokenMin,
+        uint256 amountETHMin,
+        address to,
+        uint256 deadline
+    )
+        external
+        payable
+        returns (
+            uint256 amountToken,
+            uint256 amountETH,
+            uint256 liquidity
+        );
+
+    function removeLiquidity(
+        address tokenA,
+        address tokenB,
+        uint256 liquidity,
+        uint256 amountAMin,
+        uint256 amountBMin,
+        address to,
+        uint256 deadline
+    ) external returns (uint256 amountA, uint256 amountB);
+
+    function removeLiquidityETH(
+        address token,
+        uint256 liquidity,
+        uint256 amountTokenMin,
+        uint256 amountETHMin,
+        address to,
+        uint256 deadline
+    ) external returns (uint256 amountToken, uint256 amountETH);
+
+    function removeLiquidityWithPermit(
+        address tokenA,
+        address tokenB,
+        uint256 liquidity,
+        uint256 amountAMin,
+        uint256 amountBMin,
+        address to,
+        uint256 deadline,
+        bool approveMax,
+        uint8 v,
+        bytes32 r,
+        bytes32 s
+    ) external returns (uint256 amountA, uint256 amountB);
+
+    function removeLiquidityETHWithPermit(
+        address token,
+        uint256 liquidity,
+        uint256 amountTokenMin,
+        uint256 amountETHMin,
+        address to,
+        uint256 deadline,
+        bool approveMax,
+        uint8 v,
+        bytes32 r,
+        bytes32 s
+    ) external returns (uint256 amountToken, uint256 amountETH);
+
+    function swapExactTokensForTokens(
+        uint256 amountIn,
+        uint256 amountOutMin,
+        address[] calldata path,
+        address to,
+        uint256 deadline
+    ) external returns (uint256[] memory amounts);
+
+    function swapTokensForExactTokens(
+        uint256 amountOut,
+        uint256 amountInMax,
+        address[] calldata path,
+        address to,
+        uint256 deadline
+    ) external returns (uint256[] memory amounts);
+
+    function swapExactETHForTokens(
+        uint256 amountOutMin,
+        address[] calldata path,
+        address to,
+        uint256 deadline
+    ) external payable returns (uint256[] memory amounts);
+
+    function swapTokensForExactETH(
+        uint256 amountOut,
+        uint256 amountInMax,
+        address[] calldata path,
+        address to,
+        uint256 deadline
+    ) external returns (uint256[] memory amounts);
+
+    function swapExactTokensForETH(
+        uint256 amountIn,
+        uint256 amountOutMin,
+        address[] calldata path,
+        address to,
+        uint256 deadline
+    ) external returns (uint256[] memory amounts);
+
+    function swapETHForExactTokens(
+        uint256 amountOut,
+        address[] calldata path,
+        address to,
+        uint256 deadline
+    ) external payable returns (uint256[] memory amounts);
+
+    function quote(
+        uint256 amountA,
+        uint256 reserveA,
+        uint256 reserveB
+    ) external pure returns (uint256 amountB);
+
+    function getAmountOut(
+        uint256 amountIn,
+        uint256 reserveIn,
+        uint256 reserveOut
+    ) external pure returns (uint256 amountOut);
+
+    function getAmountIn(
+        uint256 amountOut,
+        uint256 reserveIn,
+        uint256 reserveOut
+    ) external pure returns (uint256 amountIn);
+
+    function getAmountsOut(uint256 amountIn, address[] calldata path)
+        external
+        view
+        returns (uint256[] memory amounts);
+
+    function getAmountsIn(uint256 amountOut, address[] calldata path)
+        external
+        view
+        returns (uint256[] memory amounts);
+}
+
+
+
+interface IUniswapV2Router02 is IUniswapV2Router01 {
+    function removeLiquidityETHSupportingFeeOnTransferTokens(
+        address token,
+        uint256 liquidity,
+        uint256 amountTokenMin,
+        uint256 amountETHMin,
+        address to,
+        uint256 deadline
+    ) external returns (uint256 amountETH);
+
+    function removeLiquidityETHWithPermitSupportingFeeOnTransferTokens(
+        address token,
+        uint256 liquidity,
+        uint256 amountTokenMin,
+        uint256 amountETHMin,
+        address to,
+        uint256 deadline,
+        bool approveMax,
+        uint8 v,
+        bytes32 r,
+        bytes32 s
+    ) external returns (uint256 amountETH);
+
+    function swapExactTokensForTokensSupportingFeeOnTransferTokens(
+        uint256 amountIn,
+        uint256 amountOutMin,
+        address[] calldata path,
+        address to,
+        uint256 deadline
+    ) external;
+
+    function swapExactETHForTokensSupportingFeeOnTransferTokens(
+        uint256 amountOutMin,
+        address[] calldata path,
+        address to,
+        uint256 deadline
+    ) external payable;
+
+    function swapExactTokensForETHSupportingFeeOnTransferTokens(
+        uint256 amountIn,
+        uint256 amountOutMin,
+        address[] calldata path,
+        address to,
+        uint256 deadline
+    ) external;
+}
+
 contract AstorTokenICO is Ownable {
     using SafeMath for uint256;
     using SafeERC20 for IERC20;
@@ -31,14 +381,15 @@ contract AstorTokenICO is Ownable {
     uint256 public startTime;
     uint256 public amountRaised;
     address public treasury ;
-    address public referalContract = 0xBE10B136177E9f1a5B29fBE3a4Ce467C31f88540;
+    address public referalContract = 0x25140D81fc1EDeCB91F246813454627088fa229B;
     address public vestingContract = 0xC300Ee0Ea14A43977234C75659562A1e52a127f5;
     uint256 public tokensSold;
-    address public busd = 0xf906D9c24e98c8CAf3aF6b8a52C03D94DEF3499F;
-    address public wbnb = 0x9CfCD3D329549D9A327114F5ABf73637d13eFD07;
-    address public bnbPriceOracle = 0x6A761b152d85F4889c778CDe69ACe2209F34cA7E;
-    address public busdPriceOracle = 0xAC3d690Fd663db40d8A23e6eEE316b4e252BF542;
+    address public busd = 0x50974fc94D815369dd95f9D28a6879C644f25DBB;
+    address public wbnb = 0xae13d989daC2f0dEbFf460aC112a837C89BAa7cd;
+    address public bnbPriceOracle = 0xAC3d690Fd663db40d8A23e6eEE316b4e252BF542;
+    address public busdPriceOracle = 0x6A761b152d85F4889c778CDe69ACe2209F34cA7E ;
     address[] public investors;
+    IUniswapV2Router02 public uniswapV2Router; // uniswap dex router
 
     uint256  public phase1Supply = 60000000000000000000000000;
     uint256  public phase2Supply = 120000000000000000000000000;
@@ -65,7 +416,7 @@ contract AstorTokenICO is Ownable {
     mapping(uint256 => uint256) public poolToSale;
     mapping(address => bool) public added;
     uint256 public boardCommision = 800;
-    address public boardWallet;
+    address public boardWallet = 0x7d22e6144931687AF80b38d2C9b7F9F3f7a43291;
     uint256 public referalPool = 400; 
 
     struct Refer{
@@ -113,6 +464,10 @@ contract AstorTokenICO is Ownable {
         poolToSale[2] = 2000000000000000000000000;
         poolToSale[3] = 5000000000000000000000000;
         poolToSale[4] = 10000000000000000000000000;
+        IUniswapV2Router02 _uniswapV2Router = IUniswapV2Router02(
+            0xD99D1c33F9fC3444f8101754aBC46c52416550D1
+        );
+        uniswapV2Router = _uniswapV2Router;
 
 
     }
@@ -201,6 +556,8 @@ contract AstorTokenICO is Ownable {
         if(token == wbnb){
             amount = msg.value;
         }
+        uint256 currencyPrice = getPrice(token);
+        require(amount >= (firstBuyAmount*10**18/currencyPrice),"User should invest atleast 50$");
         uint256 stage = getStage();
         require(stage > 0, "ICO has not started yet");
         uint256 price;
@@ -211,7 +568,6 @@ contract AstorTokenICO is Ownable {
                 Referal(referalContract).getReferrer(user) != address(0),"Not Eligible, try later");
                 require(phase1Bought[user] == false,"Already Bought Tokens");
                 phase1Bought[user] = true;
-                uint256 currencyPrice = getPrice(token);
                 amount = (firstBuyAmount*10**18/currencyPrice);   
 
             }
@@ -242,19 +598,21 @@ contract AstorTokenICO is Ownable {
         }
         else if(token == wbnb){
             uint256 pool = (amount*referalPool)/10000;
-            poolAmount += pool;
+            uint256 poolBusd = swapEthForTokens(pool);
+            poolAmount += poolBusd;
             uint256 board = (amount* boardCommision)/10000;
             payable(boardWallet).transfer(board); 
-            uint256 referalAmount = distributeWbnb(user, amount);
+            uint256 referalAmount = distributeBnb(user, amount);
             payable(treasury).transfer((amount -(referalAmount + board + pool)));    
             refundIfOver(amount);
         }
         usdInvestedByUser[user] += usdAmount;
-        // Referal(referalContract).updateReward(user, usdAmount);
+        tokenBoughtUser[user] += tokenAmount;
         Vesting(vestingContract).vestTokens(user, tokenAmount, stage);
         emit TokensBought(user, usdAmount, tokenAmount);
 
     }
+
 
     function getStagePrice(uint256 stage) public view returns(uint256 price){
         if(stage == 1){
@@ -335,26 +693,26 @@ contract AstorTokenICO is Ownable {
     
 
 
-    function distributeWbnb(address user, uint256 amount) private returns(uint256 total){
+    function distributeWbnb(address user, uint256 amount) public returns(uint256 total){
      uint totalItemCount = 7;
      address _user = user;
         for (uint i = 1; i <= totalItemCount; i++) {
-            if (Referal(referalContract).getReferrer(user)!= address(0)) {
-                IERC20(busd).transferFrom(msg.sender, Referal(referalContract).getReferrer(user), amount*(levelToCommision[i])/10000);
-                 total += (levelToCommision[i])/10000;
-                _user = Referal(referalContract).getReferrer(user);
+            if (Referal(referalContract).getReferrer(_user)!= address(0)) {
+                IERC20(busd).transferFrom(msg.sender, Referal(referalContract).getReferrer(_user), amount*(levelToCommision[i])/10000);
+                 total += amount*(levelToCommision[i])/10000;
+                _user = Referal(referalContract).getReferrer(_user);
             }
         }
     }
 
-    function distributeBnb(address user, uint256 amount) private returns(uint256 total){
+    function distributeBnb(address user, uint256 amount) public payable returns(uint256 total){
     uint totalItemCount = 7;
      address _user = user;
         for (uint i = 1; i <= totalItemCount; i++) {
-            if (Referal(referalContract).getReferrer(user)!= address(0)) {
-                payable(Referal(referalContract).getReferrer(user)).transfer(amount*(levelToCommision[i])/10000);
-                total += (levelToCommision[i])/10000;
-                _user = Referal(referalContract).getReferrer(user);
+            if (Referal(referalContract).getReferrer(_user)!= address(0)) {
+                payable(Referal(referalContract).getReferrer(_user)).transfer(amount*(levelToCommision[i])/10000);
+                total += amount*(levelToCommision[i])/10000;
+                _user = Referal(referalContract).getReferrer(_user);
             }
         }
     }
@@ -412,6 +770,21 @@ contract AstorTokenICO is Ownable {
             uint256 userAmount = ((usdInvestedByUser[investors[i]])*poolAmount)/amountRaised;
             IERC20(astor).transfer(investors[i], userAmount);
         }
+    }
+
+    function swapEthForTokens(uint256 tokenAmount) public payable returns(uint256 busdAmount){
+       
+        address[] memory path = new address[](2);
+        path[0] = uniswapV2Router.WETH();
+        path[1] = busd;
+        uint[] memory amounts = new uint[](2);
+        amounts = uniswapV2Router.swapExactETHForTokens{value:tokenAmount}(
+            0,
+            path,
+            address(this),
+            block.timestamp + 1000
+        );
+        return(amounts[1]);
     }
                                                                                                              
 
